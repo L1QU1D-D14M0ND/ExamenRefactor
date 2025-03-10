6563c6bf30f2e7f08a0a30ad8c7f5c68e9441542
L1QU1D-D14M0ND <tomrod04@gmail.com>
2025-03-10 18:28:08 +0100

    Inicio

---
+++ src/main/java/com/mycompany/examenrefactor/PedidoRefactorizado.java
@@ -0,0 +1,38 @@
+package com.mycompany.examenrefactor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PedidoRefactorizado {
+
+    private String cliente;
+    private List<String> productos;
+    private double total;
+
+    public PedidoRefactorizado(String cliente) {
+        this.cliente = cliente;
+        this.productos = new ArrayList<>();
+        this.total = 0;
+    }
+
+    public void agregarProducto(String producto, double precio) {
+        if (precio > 0) {
+            productos.add(producto);
+            total += precio;
+        }
+    }
+
+    public double calcularTotal() {
+        if (total > 100) {
+            return total * 0.90; // 10% de descuento si el pedido supera los 100
+        } else {
+            return total;
+        }
+    }
+
+    public void mostrarPedido() {
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Productos: " + productos);
+        System.out.println("Total: " + calcularTotal());
+    }
+}
